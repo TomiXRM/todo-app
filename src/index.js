a330e9dcd3c3c46c29515d3233e838cc5da2c882
Daiki Tomioka <chocolate0785lego@icloud.com>
2021-02-16 02:10:07 +0900

    add : remove from done list

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import "./styles.css";
 const onClickAdd = () => {
   const inputText = document.getElementById("add-text").value;
   if (!inputText == false) {
-    document.getElementById("add-text").value = null;
+    document.getElementById("add-text").value = null; ///テキストボックスのクリア
+
     //divタグの生成
     const div = document.createElement("div");
     div.className = "list-row"; //プロパティadd class
@@ -13,21 +14,43 @@ const onClickAdd = () => {
     li.innerText = inputText; //プロパティ　入力内容　add
     console.log(li);
 
-    //button
-    const doneBtn = document.createElement("button");
-    doneBtn.innerText = "done";
-    doneBtn.addEventListener("click", () => {
-      alert("done!!");
-    });
-
+    //未完了
+    //button removeの追加
     const deleteBtn = document.createElement("button");
     deleteBtn.innerText = "delete";
     deleteBtn.addEventListener("click", () => {
-      // alert("delete");
       //消す処理 押されたボタンの親タグを未完から削除
-      const deleteTarget = deleteBtn.parentNode;
-      document.getElementById("imcomplete-list").removeChild(deleteTarget);
-      console.log(deleteTarget);
+      deleteTODO("imcomplete-list", deleteBtn.parentNode);
+      // console.log(deleteTarget);
+    });
+
+    //bottn move to done
+    const doneBtn = document.createElement("button");
+    doneBtn.innerText = "done";
+    doneBtn.addEventListener("click", () => {
+      const addtarget = doneBtn.parentNode;
+      deleteTODO("imcomplete-list", addtarget);
+      const text = addtarget.firstElementChild.innerText; //取得
+      addtarget.textContent = null;
+
+      const li = document.createElement("li");
+      li.innerText = text;
+
+      const removeButton = document.createElement("button");
+      removeButton.innerText = "remove";
+      removeButton.addEventListener("click", () => {
+        deleteTODO("complete-list", removeButton.parentNode);
+      });
+
+      const backButton = document.createElement("button");
+      backButton.innerText = "back";
+      // backButton.addEventListener(() => {});
+
+      addtarget.appendChild(li);
+
+      addtarget.appendChild(backButton);
+      addtarget.appendChild(removeButton);
+      document.getElementById("complete-list").appendChild(addtarget);
     });
 
     //divの中にliとかを入れる
@@ -40,6 +63,10 @@ const onClickAdd = () => {
     console.log(div);
   }
   // alert(inputText);
+};
+
+const deleteTODO = (from, target) => {
+  document.getElementById(from).removeChild(target);
 };
 
 document
