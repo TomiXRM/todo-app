a5b6ccb5601ac513633fd9da467b60cb91e3db1e
Daiki Tomioka <chocolate0785lego@icloud.com>
2021-02-16 01:00:39 +0900

    add  : TODO add function

--- src/index.js
+++ src/index.js
@@ -3,7 +3,30 @@ import "./styles.css";
 const onClickAdd = () => {
   const inputText = document.getElementById("add-text").value;
   document.getElementById("add-text").value = null;
-  alert(inputText);
+  //divタグの生成
+  const div = document.createElement("div");
+  div.className = "list-row"; //プロパティadd class
+
+  //li タグ
+  const li = document.createElement("li");
+  li.innerText = inputText; //プロパティ　入力内容　add
+  console.log(li);
+
+  //button
+  const btn1 = document.createElement("button");
+  btn1.innerText = "done";
+  const btn2 = document.createElement("button");
+  btn2.innerText = "delete";
+
+  //divの中にliとかを入れる
+  div.appendChild(li);
+  div.appendChild(btn1);
+  div.appendChild(btn2);
+
+  //未完了のリストに追加
+  document.getElementById("incomplete-list").appendChild(div);
+  console.log(div);
+  // alert(inputText);
 };
 
 document
