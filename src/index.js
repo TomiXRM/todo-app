57ee9b7dd04be913c033105dae6b7550ceb844c5
Daiki Tomioka <chocolate0785lego@icloud.com>
2021-02-16 00:50:27 +0900

    add js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import "./styles.css";
+
+const onClickAdd = () => {
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = null;
+  alert(inputText);
+};
+
+document
+  .getElementById("add-botton")
+  .addEventListener("click", () => onClickAdd());
